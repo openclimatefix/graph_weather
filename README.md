a9bd0a6a1c564c68ba578651e4575c938a17cbbd
Jacob Bieker <jacob@bieker.tech>
2022-02-21 12:44:21 +0000

    Add outline of model

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# graph_weather
-Implementation of the Graph Weather paper (https://arxiv.org/pdf/2202.07575.pdf) in PyTorch
+# Graph Weather
+Implementation of the Graph Weather paper (https://arxiv.org/pdf/2202.07575.pdf) in PyTorch.
+
+
+## Installation
+
+This library can be installed through
+
+```bash
+pip install graph_weather
+```
+
+## Pretrained Weights
+Coming soon! We plan to train a model on GFS 0.25 degree operational forecasts, as well as MetOffice NWP forecasts.
+We also plan trying out adaptive meshes, and predicting future satellite imagery as well.
+
+## Training Data
+Training data will be available through HuggingFace Datasets for the GFS forecasts. MetOffice NWP forecasts we cannot 
+redistribute, but can be accessed through [CEDA](https://data.ceda.ac.uk/).
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+install_requires = (this_directory / "requirements.txt").read_text().splitlines()
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="graph_weather",
+    version="0.0.1",
+    packages=find_packages(),
+    url="https://github.com/openclimatefix/graph_weather",
+    license="MIT License",
+    company="Open Climate Fix Ltd",
+    author="Jacob Bieker",
+    install_requires=install_requires,
+    long_description=long_description,
+    ong_description_content_type="text/markdown",
+    author_email="jacob@openclimatefix.org",
+    description="Weather Forecasting with Graph Neural Networks",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
