542ad5084339ed0d7bbcb9055349ea023000d4c1
Jacob Bieker <jacob@bieker.tech>
2022-03-03 14:11:37 +0000

    Add in cos(lat) weighting

--- graph_weather/models/losses.py
+++ graph_weather/models/losses.py
@@ -1,5 +1,5 @@
 import torch
-from torch.nn.functional import mse_loss
+import numpy as np
 
 """
 
@@ -26,7 +26,7 @@ Calculate 1. first beforehand with the sets of variables, then use that unit var
 
 
 class NormalizedMSELoss(torch.nn.Module):
-    def __init__(self, feature_variance: list):
+    def __init__(self, feature_variance: list, lat_lons: list):
         """
         Normalized MSE Loss as described in the paper
 
@@ -35,17 +35,25 @@ class NormalizedMSELoss(torch.nn.Module):
         sigma_t_3hr, where sigma^2_T,3hr is the variance of the 3 hour change in temperature,
          averaged across space (lat/lon + pressure levels) and time (100 random temporal frames).
 
+         Additionally weights by the cos(lat) of the feature
+
         Args:
             feature_variance: Variance for each of the physical features
+            lat_lons: List of lat/lon pairs, used to generate weighting
         """
         super().__init__()
         self.feature_variance = torch.tensor(feature_variance)
+        weights = []
+        for lat, lon in lat_lons:
+            weights.append(np.cos(lat))
+        self.weights = torch.from_numpy(weights)
 
     def forward(self, pred: torch.Tensor, target: torch.Tensor):
         """
         Calculate the loss
 
         Rescales both predictions and target, so assumes neither are already normalized
+        Additionally weights by the cos(lat) of the set of features
 
         Args:
             pred: Prediction tensor
@@ -57,4 +65,8 @@ class NormalizedMSELoss(torch.nn.Module):
 
         pred = pred / self.feature_variance
         target = target / self.feature_variance
-        return mse_loss(pred, target)
+
+        out = (pred - target)**2
+        # Weight by the latitude, as that changes, so does the size of the pixel
+        out = out * self.weights.expand_as(out)
+        return out.sum(0)
