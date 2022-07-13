db1f3941b6c38a001f689224303d03e56c528ba0
Jacob Bieker <jacob@bieker.tech>
2022-07-13 14:21:58 +0100

    Offline for both

--- train/pl_graph_weather.py
+++ train/pl_graph_weather.py
@@ -82,7 +82,7 @@ def get_mean_stds():
         stds[f"{i}m_above_ground"] = np.stack(stds[f"{i}m_above_ground"], axis=-1)
     return means, stds
 
-hf_ds = datasets.load_dataset("openclimatefix/gfs-surface-pressure-2deg", split='train', streaming=True)
+hf_ds = datasets.load_dataset("openclimatefix/gfs-surface-pressure-2deg", split='train', streaming=False)
 example_batch = next(iter(hf_ds))
 means, stds = get_mean_stds()
 """
