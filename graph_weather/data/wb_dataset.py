42fe3586dab41493796d516bbdb472f03752e1c9
Mihai Alexe <email@email.com>
2022-08-12 11:27:14 +0000

    Dataloader + unit test

--- graph_weather/data/wb_dataset.py
+++ graph_weather/data/wb_dataset.py
@@ -17,6 +17,7 @@ class WeatherBenchDataset(IterableDataset):
     Iterable dataset for WeatherBench data.
     Design inspired by https://github.com/openclimatefix/predict_pv_yield/blob/main/notebooks/20.0_simplify_data_loading.ipynb.
     """
+
     # TODO: refactor this hardcoded value
     nlev = 13
 
@@ -42,13 +43,18 @@ class WeatherBenchDataset(IterableDataset):
         """
         self.fnames = fnames
         self.ds: Optional[xr.Dataset] = None
+
         self.lead_time = lead_time
+        assert self.lead_time > 0 and self.lead_time % 6 == 0, "Lead time must be multiple of 6 hours"
+        self.lead_step = lead_time // 6
+
         self.vars = var_names
         self.nvar = len(self.vars)
-        self.bs = batch_chunk_size
+        self.bcs = batch_chunk_size
         self.read_wb_data = read_wb_data_func
         self.mean = var_mean
         self.sd = var_sd
+
         # lazy init
         self.n_samples_per_epoch_total: int = 0
         self.n_samples_per_epoch_per_worker: int = 0
@@ -61,8 +67,13 @@ class WeatherBenchDataset(IterableDataset):
         self.ds = self.ds[self.vars]
 
         assert self.nlev == len(self.ds.level), "Incorrect number of pressure levels!"
-        self.effective_ds_len = int(np.ceil((len(self.ds.time) - self.lead_time) / self.bs))
-        self.n_samples_per_epoch_per_worker = self.effective_ds_len // n_workers
+
+        self.ds_len = len(self.ds.time) - self.lead_step
+        print(f"Dataset length: {self.ds_len} ...")
+        self.effective_ds_len = int(np.ceil(self.ds_len / self.bcs))
+        print(f"Batch chunk size: {self.bcs} -- effective dataset length: {self.effective_ds_len} ...")
+        self.n_chunks_per_worker = self.effective_ds_len // n_workers
+        print(f"Each worker gets {self.n_chunks_per_worker} batch chunks per epoch...")
 
         # each worker must have a different seed for its random number generator,
         # otherwise all the workers will output exactly the same data
@@ -72,14 +83,27 @@ class WeatherBenchDataset(IterableDataset):
         return (data - self.mean) / self.sd
 
     def __iter__(self):
-        LOGGER.debug(f"n_samples_per_epoch_per_worker: {self.n_samples_per_epoch_per_worker}")
-        for _ in range(self.n_samples_per_epoch_per_worker):
-            i = self.rng.integers(low=0, high=self.effective_ds_len, dtype=np.uint32)
+        worker_info = torch.utils.data.get_worker_info()
 
-            start, end = i * self.bs, (i + 1) * self.bs
+        if worker_info is None:
+            low = 0
+            high = self.ds_len
+        else:
+            worker_id = worker_info.id
+            low = worker_id * self.n_chunks_per_worker
+            high = min((worker_id + 1) * self.n_chunks_per_worker, self.ds_len)
+            print(f"Worker ID {worker_id} owns range {low} -- {high} ...")
+
+        chunk_index_range = np.arange(low, high + 1, dtype=np.uint32)
+        shuffled_chunk_indices = self.rng.choice(chunk_index_range, size=self.n_chunks_per_worker, replace=False)
+
+        for i in shuffled_chunk_indices:
+            print(f"Worker ID {worker_id} sampled index {i} ...")
+
+            start, end = i * self.bcs, (i + 1) * self.bcs
             Xv_ = self._transform(self.ds.isel(time=slice(start, end)))
 
-            start, end = i * self.bs + self.lead_time, (i + 1) * self.bs + self.lead_time
+            start, end = i * self.bcs + self.lead_step, (i + 1) * self.bcs + self.lead_step
             Yv_ = self._transform(self.ds.isel(time=slice(start, end)))
 
             # shape: (bs, nvar, nlev, lat, lon)
@@ -98,100 +122,3 @@ def worker_init_func(worker_id: int) -> None:
     else:
         dataset_obj = worker_info.dataset  # the copy of the dataset held by this worker process.
         dataset_obj.per_worker_init(n_workers=worker_info.num_workers)
-
-
-# class WeatherBenchDataset(Dataset):
-#     """
-#     Map-style dataset that returns individual batches rather than single samples.
-#     A mini-batch is assembled from several "chunks" (not related to the Dask chunks).
-#     The number of chunks in a batch should be set through the DataLoader.
-#     This allows us to randomly shuffle chunks inside a batch, hence individual batches will be different between epochs.
-#     """
-
-#     # chunk size for dask arrays along the time dimension
-#     # larger chunk size will lead to weird errors, so keep this small
-#     __dask_time_chunk = 10
-
-#     def __init__(
-#         self,
-#         fnames: List[str],
-#         var_names: List[str],
-#         lead_time: int = 6,
-#         batch_chunk_size: int = 4,
-#         var_means: Optional[xr.Dataset] = None,
-#         var_std: Optional[xr.Dataset] = None,
-#         dask_client: Optional[Client] = None,
-#         persist_in_memory: bool = False,
-#     ) -> None:
-#         """
-#         Args:
-#             fnames: list of data files to open
-#             var_names: list of variable names to retain
-#             lead_time: lead time, in hours (must be a multiple of 6)
-#             batch_chunk_size: size of a batch chunk
-#             var_means: pre-computed means
-#             var_std: pre-computed standard deviations
-#             dask_client: dask Client, used for parallel data processing, batching, etc.
-#             persist_in_memory: if True and (dask_client is not None), persist dataset in RAM, distributed across all Dask workers
-#                                if True and (dask_client == None), we simply load the data in the memory of the current process
-#         """
-#         super().__init__()
-#         self.vars = var_names
-#         self.bs = batch_chunk_size
-#         # Dask client (parallel data processing)
-#         self.dask_client = dask_client
-
-#         # assumes hourly data, otherwise the length is incorrect
-#         if (lead_time <= 0) or (lead_time % 6 != 0):
-#             raise RuntimeError(f"Lead time = {lead_time}, but it must be a (positive) multiple of 6!")
-#         self.lead_time = lead_time
-
-#         # open data files and retain only the variables we need
-#         self.ds = xr.open_mfdataset(fnames, parallel=True, chunks={"time": self.__dask_time_chunk})
-#         self.ds = self.ds[var_names]
-
-#         self.nvar = len(self.vars)
-#         self.nlev = len(self.ds.level)
-#         self.nlat, self.nlon = len(self.ds.latitude), len(self.ds.longitude)
-
-#         if __DEBUG__:
-#             # useful when debugging: retain only the first N_TIMES samples
-#             self.ds = self.ds.isel(time=slice(None, N_TIMES))
-
-#         self.length = int(np.ceil((len(self.ds.time) - lead_time) / self.bs))
-#         print(f"Dataset length: {self.length}")
-
-#         # this will trigger the computation of the mean and std (if needed)
-#         if persist_in_memory:
-#             self.ds = self.__persist(self.ds)
-
-#         # normalization (mu, std)
-#         self.mean = self.ds.mean().compute() if var_means is None else var_means
-#         self.std = self.ds.std("time").mean(("level", "latitude", "longitude")).compute() if var_std is None else var_std
-
-#     def _transform(self, data: xr.Dataset) -> xr.Dataset:
-#         return (data - self.mean) / self.std
-
-#     def __len__(self):
-#         """Returns the length of the dataset"""
-#         return self.length
-
-#     def __persist(self, ds: xr.Dataset) -> xr.Dataset:
-#         return self.dask_client.persist(ds) if self.dask_client is not None else ds.load()
-
-#     def __getitem__(self, i: int) -> Tuple[np.ndarray, ...]:
-#         return self.__get_mini_batch_chunk(i)
-
-#     def __get_mini_batch_chunk(self, i) -> Tuple[np.ndarray, ...]:
-#         """Returns (part of) a mini-batch"""
-#         start, end = i * self.bs, (i + 1) * self.bs
-#         Xv_ = self._transform(self.ds.isel(time=slice(start, end)))
-
-#         start, end = i * self.bs + self.lead_time, (i + 1) * self.bs + self.lead_time
-#         Yv_ = self._transform(self.ds.isel(time=slice(start, end)))
-
-#         # shape: (bs, nvar, nlev, lat, lon)
-#         X = da.stack([Xv_[var] for var in self.vars], axis=1)
-#         Y = da.stack([Yv_[var] for var in self.vars], axis=1)
-
-#         return X, Y
---
+++ tests/test_dataloader.py
@@ -0,0 +1,160 @@
+from typing import List, Tuple
+import unittest
+import logging
+
+import numpy as np
+import xarray as xr
+import dask.array as da
+from einops import rearrange
+
+import torch
+from torch.utils.data import DataLoader
+
+from graph_weather.data.wb_dataset import WeatherBenchDataset, worker_init_func
+
+# dummy xarray data
+np.random.seed(0)
+
+NTIME = 100
+NLON, NLAT = 2, 2
+PLEV = 13
+NVAR = 1
+BATCH_SIZE = 2
+BATCH_CHUNK_SIZE = 3
+
+lon = [[-9.83, -9.32], [-9.79, -9.23]]
+lat = [[42.25, 42.21], [42.63, 42.59]]
+time = np.array(range(NTIME))
+plev = np.array(range(PLEV))
+
+# init t2m values with the time index value (should make it easier to identify later)
+t2m = np.zeros((NTIME, PLEV, NLAT, NLON), dtype=np.int32)
+for i in range(NTIME):
+    t2m[i, ...] = np.ones((PLEV, NLAT, NLON)) * i
+print(t2m.min(), t2m.max())
+assert (t2m >= 0).all()
+
+
+DATA = xr.Dataset(
+    data_vars=dict(
+        t2m=(["time", "level", "x", "y"], t2m),
+    ),
+    coords=dict(
+        lon=(["x", "y"], lon),
+        lat=(["x", "y"], lat),
+        time=time,
+        level=plev,
+    ),
+    attrs=dict(description="Dummy data."),
+)
+
+t2m_dummy_mean = np.zeros((NTIME, PLEV, NLAT, NLON), dtype=np.int32)
+t2m_dummy_sd = np.ones((NTIME, PLEV, NLAT, NLON), dtype=np.int32)
+
+DATA_MEANS = xr.Dataset(
+    data_vars=dict(
+        t2m=(["time", "level", "x", "y"], t2m_dummy_mean),
+    ),
+    coords=dict(
+        lon=(["x", "y"], lon),
+        lat=(["x", "y"], lat),
+        time=time,
+        level=plev,
+    ),
+    attrs=dict(description="Dummy data mean."),
+)
+
+DATA_SDS = xr.Dataset(
+    data_vars=dict(
+        t2m=(["time", "level", "x", "y"], t2m_dummy_sd),
+    ),
+    coords=dict(
+        lon=(["x", "y"], lon),
+        lat=(["x", "y"], lat),
+        time=time,
+        level=plev,
+    ),
+    attrs=dict(description="Dummy data sd."),
+)
+
+
+def test_batch_collator(batch_data: List[Tuple[np.ndarray, np.ndarray]]) -> Tuple[torch.Tensor, torch.Tensor]:
+    """
+    Custom collation function. It collates several batch chunks into a "full" batch.
+    Args:
+        data: batch data, [(X_chunk0, Y_chunk0), (X_chunk1, Y_chunk1), ...]
+              with X_chunk0.shape == (batch_chunk_size, nvars, nlevels, lat, lon)
+    """
+    X, Y = list(zip(*batch_data))
+    return torch.as_tensor(
+        # reshape to (bs, (lat*lon), (nvar * nlev))
+        rearrange(da.concatenate([x for x in X], axis=0).compute(), "b v l h w -> b (h w) (v l)"),
+        dtype=torch.int32,
+    ), torch.as_tensor(
+        rearrange(da.concatenate([y for y in Y], axis=0).compute(), "b v l h w -> b (h w) (v l)"),
+        dtype=torch.int32,
+    )
+
+
+def read_dummy_data(fnames: List[str]) -> xr.Dataset:
+    del fnames  # not used
+    return DATA
+
+
+class DataloaderTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.yyyymmdd = "20220812"
+        cls.hh = "00"
+        logging.disable(logging.CRITICAL)  # disable logging
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_dataloader(self):
+        ds_test = WeatherBenchDataset(
+            fnames=[],  # the data is already in memory
+            read_wb_data_func=read_dummy_data,
+            var_names=["t2m"],
+            var_mean=DATA_MEANS,
+            var_sd=DATA_SDS,
+            lead_time=6,
+            batch_chunk_size=BATCH_CHUNK_SIZE,
+        )
+
+        dl_test = DataLoader(
+            ds_test,
+            # we're putting together one full batch from this many batch-chunks
+            # this means the "real" batch size == config["model:dataloader:batch-size"] * config["model:dataloader:batch-chunk-size"]
+            batch_size=BATCH_SIZE,
+            # number of worker processes
+            num_workers=2,
+            # use of pinned memory can speed up CPU-to-GPU data transfers
+            # see https://pytorch.org/docs/stable/notes/cuda.html#cuda-memory-pinning
+            pin_memory=True,
+            # custom collator (see above)
+            collate_fn=test_batch_collator,
+            # worker initializer
+            worker_init_fn=worker_init_func,
+            # prefetch batches (default prefetch_factor == 2)
+            prefetch_factor=2,
+            # drop last incomplete batch (makes it easier to test the resulting tensor shapes)
+            drop_last=True,
+        )
+
+        print("******* Iteration 1 ... *******")
+        for i_batch, batch in enumerate(dl_test):
+            # check batch shapes
+            self.assertEqual(batch[0].shape, batch[1].shape)
+            self.assertEqual(batch[0].shape, (BATCH_SIZE * BATCH_CHUNK_SIZE, NLAT * NLON, PLEV * NVAR))
+            X, Y = batch
+            print(f"Batch index {i_batch:02d} -- X: {X[:, 0, 0].tolist()} -- Y: {Y[:, 0, 0].tolist()}")
+
+        print("******* Iteration 2 ... *******")
+        for i_batch, batch in enumerate(dl_test):
+            # check batch shapes
+            self.assertEqual(batch[0].shape, batch[1].shape)
+            self.assertEqual(batch[0].shape, (BATCH_SIZE * BATCH_CHUNK_SIZE, NLAT * NLON, PLEV * NVAR))
+            X, Y = batch
+            print(f"Batch index {i_batch:02d} -- X: {X[:, 0, 0].tolist()} -- Y: {Y[:, 0, 0].tolist()}")
