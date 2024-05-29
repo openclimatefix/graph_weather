47d0d481788767583315615d68117a314b30c0e1
Lorenzo Breschi <lawrencebreschi@gmail.com>
2024-05-29 10:58:50 +0200

    fengwu_ghr: initial

--- graph_weather/models/__init__.py
+++ graph_weather/models/__init__.py
@@ -6,3 +6,4 @@ from .layers.assimilator_encoder import AssimilatorEncoder
 from .layers.decoder import Decoder
 from .layers.encoder import Encoder
 from .layers.processor import Processor
+from .fengwu_ghr.layers import MetaModel
