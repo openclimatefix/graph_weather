c57a27ec2358d5cd2ed0883d1f406efa52806a7b
Lorenzo Breschi <lawrencebreschi@gmail.com>
2024-06-11 15:59:59 +0200

    MetaModel initial

--- graph_weather/models/fengwu_ghr/layers.py
+++ graph_weather/models/fengwu_ghr/layers.py
@@ -1,3 +1,6 @@
+from scipy.interpolate import griddata
+from torch_geometric.nn import knn
+from torch_geometric.utils import scatter
 import numpy as np
 from scipy.interpolate import griddata, interpn
 import torch
@@ -12,9 +15,6 @@ from torch import nn
 def pair(t):
     return t if isinstance(t, tuple) else (t, t)
 
-from torch_geometric.nn import knn
-from torch_geometric.utils import scatter
-
 
 def knn_interpolate(x: torch.Tensor, pos_x: torch.Tensor, pos_y: torch.Tensor,
                     k: int = 3, num_workers: int = 1):
@@ -26,17 +26,16 @@ def knn_interpolate(x: torch.Tensor, pos_x: torch.Tensor, pos_y: torch.Tensor,
         squared_distance = (diff * diff).sum(dim=-1, keepdim=True)
         weights = 1.0 / torch.clamp(squared_distance, min=1e-16)
 
-    
     # print((x[x_idx]*weights).shape)
     # print(weights.shape)
     den = scatter(weights, y_idx, 0, pos_y.size(0), reduce='sum')
     # print(den.shape)
     y = scatter(x[x_idx] * weights, y_idx, 0, pos_y.size(0), reduce='sum')
-    
-    
+
     y = y / den
 
     return y
+
 
 def grid_interpolate(lat_lons: list, z: torch.Tensor,
                      height, width,
@@ -149,6 +148,7 @@ class Transformer(nn.Module):
             x = ff(x) + x
         return self.norm(x)
 
+
 class ImageMetaModel(nn.Module):
     def __init__(self, *,
                  image_size,
@@ -205,6 +205,7 @@ class ImageMetaModel(nn.Module):
 
         return x
 
+
 class MetaModel(nn.Module):
     def __init__(self, lat_lons: list, *,
                  patch_size, depth,
@@ -213,19 +214,60 @@ class MetaModel(nn.Module):
                  channels=3, dim_head=64,
                  interp_method='cubic'):
         super().__init__()
-        resolution = pair(resolution)
-        b=3
-        n=len(lat_lons)
-        d=7
-        x=torch.randn((b,n,d))
-        x=rearrange(x,"b n d -> n (b d)")
-        
-        pos_x= torch.tensor(lat_lons)
-        pos_y = torch.cartesian_prod(
-            torch.arange(0.5,resolution[0],1),
-            torch.arange(0.5,resolution[1],1)
+        self.resolution = pair(resolution)
+
+        self.pos_x = torch.tensor(lat_lons)
+        self.pos_y = torch.cartesian_prod(
+            torch.arange(0, self.resolution[0], 1),
+            torch.arange(0, self.resolution[1], 1)
         )
-        x = knn_interpolate(x,pos_x,pos_y)
-        x = rearrange(x,"m (b d) -> b m d", b=b,d=d)
+
+        self.image_model = ImageMetaModel(image_size=resolution,
+                                          patch_size=patch_size,
+                                          depth=depth,
+                                          heads=heads,
+                                          mlp_dim=mlp_dim,
+                                          channels=channels,
+                                          dim_head=dim_head)
+
+    def forward(self, x):
+        b, n, c = x.shape
+
+        x = rearrange(x, "b n c -> n (b c)")
+        x = knn_interpolate(x, self.pos_x, self.pos_y)
+        x = rearrange(x, "(h w) (b c) -> b c h w", b=b, c=c,
+                      w=self.resolution[0],
+                      h=self.resolution[1])
+
+        x = self.image_model(x)
+
+        x = rearrange(x, "b c h w -> (h w) (b c)")
+        x = knn_interpolate(x, self.pos_y, self.pos_x)
+        x = rearrange(x, "n (b c) -> b n c", b=b, c=c)
+
+        return x
+
+
+class MetaModel2(nn.Module):
+    def __init__(self, lat_lons: list, *,
+                 patch_size, depth,
+                 heads, mlp_dim,
+                 resolution=(721, 1440),
+                 channels=3, dim_head=64,
+                 interp_method='cubic'):
+        super().__init__()
+        resolution = pair(resolution)
+        b = 3
+        n = len(lat_lons)
+        d = 7
+        x = torch.randn((b, n, d))
+        x = rearrange(x, "b n d -> n (b d)")
+
+        pos_x = torch.tensor(lat_lons)
+        pos_y = torch.cartesian_prod(
+            torch.arange(0, resolution[0], 1),
+            torch.arange(0, resolution[1], 1)
+        )
+        x = knn_interpolate(x, pos_x, pos_y)
+        x = rearrange(x, "m (b d) -> b m d", b=b, d=d)
         print(x.shape)
-       
