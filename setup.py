98b9248cba261ecbbe2cb8cf0cd2f64bd1a88783
BumpVersion Action <bumpversion@github-actions>
2025-01-20 17:46:01 +0000

    Bump version: 1.0.84 → 1.0.85 [skip ci]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="graph_weather",
-    version="1.0.84",
+    version="1.0.85",
     packages=find_packages(),
     url="https://github.com/openclimatefix/graph_weather",
     license="MIT License",
