c81b9bf856932c85e1d06041069aee911f4496bc
Yuvraaj Narula <49155095+yuvraajnarula@users.noreply.github.com>
2025-03-13 22:54:41 +0530

    WeatherStationReader Processing & Add Comprehensive Tests (#141)
    * station reader and it's tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * QC defaults added, removed test dataset and create empty function and refactored test function
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Prehooks formatting, SynopticPy conditional import and handling of unexpected response format
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Ruff prehooks fixes for weather_station_reader.py
    
    * Ruff prehooks fixes for weather_station_reader.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Test_read_weatherreal_file assert fix and ruff checks
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * ruff checks
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update graph_weather/data/weather_station_reader.py
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Jacob Prince-Bieker <jacob@bieker.tech>

--- graph_weather/data/__init__.py
+++ graph_weather/data/__init__.py
@@ -4,3 +4,4 @@ from .nnja_ai import SensorDataset, collate_fn
 from .weather_station_reader import WeatherStationReader
 
 from .anemoi_dataloader import AnemoiDataset
+from .weather_station_reader import WeatherStationReader
