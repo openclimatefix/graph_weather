8ab2590780d0430795cee663a9dfb8300598ebf2
SIdharth <sid250581@gmail.com>
2026-01-16 18:10:57 +0530

    Openmeteo data integration

--- graph_weather/data/__init__.py
+++ graph_weather/data/__init__.py
@@ -2,4 +2,5 @@
 
 from .anemoi_dataloader import AnemoiDataset
 from .nnja_ai import SensorDataset
+from .openmeteo_fetcher import OpenMeteoWeatherDataFetcher
 from .weather_station_reader import WeatherStationReader
